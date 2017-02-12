65761fdc207089688fa5c998d76b23bf14b2679c
Anonrate <Anonrate@outlook.com>
2017-02-11 20:40:49 -0500

    Added project header and debug header.

---
+++ inc/dbgcstdior.h
@@ -0,0 +1,42 @@
+/**
+ * @file	dbgcstdior.h
+ * @brief	dbgcstdior.h
+ * @version	v.1
+ * @date	02/11/2017 20:13:39
+ * @author	Anonrate
+ * @copyright	
+ * 	\parblock
+ * 		GNU General Public License
+ *
+ * 		Copyright (C) 2017 Anonrate
+ *
+ * 		This program is free software: you can redistribute it and/or modify 
+ * 		it under the terms of the GNU General Public License as published by 
+ * 		the Free Software Foundation, either version 3 of the License, or 
+ * 		(at your option) any later version.
+ *
+ * 		This program is distributed in the hope that it will be useful, 
+ * 		but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * 		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * 		GNU General Public License for more details.
+ *
+ * 		You should have received a copy of the GNU General Public License
+ * 		along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 	\endparblock
+ */
+
+#ifndef DBGCSTDIOR_H
+#define DBGCSTDIOR_H  /**< Include guard for 'dbgcstdior.h'. */
+
+/**
+ * @brief   debug format file exists
+ * @details Used when a file exists.
+ */
+#define DBGF_FE   "File '%s' exists.\n"
+
+/**
+ * @brief   debug format file not exist.
+ * @details Used when a file does not exist.
+ */
+#define DBGF_FNE  "File '%s' does not exist.\n"
+#endif  /* DBGCSTDIOR_H */
